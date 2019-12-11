80a988660462c4593671b591960ec570d6bc1c34
mizu0327 <yg0327955@gmail.com>
2019-12-11 17:16:49 +0900

    Setup routing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,28 @@
 class PostsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+
+  end
+
+  def edit
+  end
+
+  def show
+  end
+  
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:shop_name, :shop_address, :description, :image)
+  end
 end
